e39a5c0fa3d1cb231611f65ab1004ef4150135e5
jdu211861 <211861t@jdu.uz>
2023-04-12 11:55:46 +0500

    inital commit

---
+++ jdu/about.php
@@ -0,0 +1,14 @@
+<?php
+ include "header.php";
+?>
+<h1>About</h1>
+
+   
+    <h3>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim culpa adipisci qui, nam iusto eligendi illo. Suscipit perspiciatis id fugiat maiores numquam porro laboriosam recusandae magnam, voluptatibus optio. Sunt, nulla?
+    </h3>
+   
+    
+<?php
+ include "footer.php";
+?>
---
+++ jdu/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  
+   
+    <nav>
+        <ul>
+            <li><a href="http://localhost:8000/index.html">Home</a></li>
+            <li><a href="http://localhost:8000/about.php">About</a></li>
+            <li><a href="http://localhost:8000/servises.php">Servises</a></li>
+            <li><a href="http://localhost:8000/Contact.php">Contact</a></li>
+            <li><a href="http://localhost:8000/kim.php">Kim</a></li>
+            <li><a href="http://localhost:8000/men.php">Men</a></li>
+            <li><a href="http://localhost:8000/sen.php">Sen</a></li>
+        </ul>
+    </nav>
